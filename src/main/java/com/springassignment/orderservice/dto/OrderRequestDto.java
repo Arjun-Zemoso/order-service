8a5d1dd9e6dda1afcd35aeb982c3f9b0b8c38c0a
Arjun-Zemoso <arjun.singh@zemosolabs.com>
2023-06-16 14:03:26 +0530

    Initial commit

---
+++ src/main/java/com/springassignment/orderservice/dto/OrderRequestDto.java
@@ -0,0 +1,19 @@
+package com.springassignment.orderservice.dto;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderRequestDto {
+    private List<Long> bookId;
+    private BigDecimal orderAmount;
+}
+
+
---
+++ src/main/java/com/springassignment/orderservice/model/Order.java
@@ -0,0 +1,24 @@
+package com.springassignment.orderservice.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @ElementCollection
+    private List<Long> bookId;
+    private BigDecimal orderAmount;
+}
